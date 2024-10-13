e756b38c5e99e36b2db865fe43a3764000f35135
eeh0046 <eeh0046@auburn.edu>
2024-10-13 12:45:08 -0500

    Add files via upload

---
+++ Hey_Day_Bonus.sh
@@ -0,0 +1,11 @@
+#shebang line
+#! /bin/bash
+
+#User is prompted to enter their name.
+echo "Happy Hey Day! Please input your name."
+
+#Input from user is assigned to the "name" variable.
+read name
+
+#User is greeted by name.
+echo "Hey $name! War Eagle!"
